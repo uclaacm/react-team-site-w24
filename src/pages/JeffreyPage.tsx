cb145fb97ed47e28bf407776334d490a4f8fd68a
Jeffrey <jeffreyliu459@gmail.com>
2024-02-13 18:44:59 -0800

    hello
    commiting and pushing changes

---
+++ src/pages/JeffreyPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const JeffreyPage: React.FC = () => {
+  return (
+    <div style={{ padding: '20px' }}>
+      Jeffrey's Page
+    </div>
+  );
+}
+
+export default JeffreyPage
