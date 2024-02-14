3f00aa5a0fa1a4a3dda066dd24603f8250bc3e1f
nkerr23 <ninarkerr@gmail.com>
2024-02-13 18:40:38 -0800

    created nina webpage

---
+++ src/pages/NinaPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NinaPage: React.FC = () => {
+  return (
+    <div style={{ padding: '20px' }}>
+      Nina's Page
+    </div>
+  );
+}
+
+export default NinaPage;
